d926494d7734de1fee5035d12194c5bcad7010ca
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-06-27 21:56:08 +0300

    port and servername

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 db_dir = os.path.abspath(os.path.join(basedir, 'post.db'))
 
 DEBUG = os.environ.get('DEBUG') == 'True'
-PORT = int(os.environ.get('PORT', 5000))
-
+port = int(os.environ.get('PORT', 5000))
+SERVER_NAME = '127.0.0.1:{}'.format(port)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_dir
 SQLALCHEMY_TRACK_MODIFICATIONS = False
