8045585266059f770ef81269c9345ae50be19365
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-06-27 21:26:06 +0300

    remeve class, sqlife only

--- config.py
+++ config.py
@@ -2,23 +2,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_dir = os.path.abspath(os.path.join(basedir, 'post.db'))
 
-class Config(object):
-    # CSRF_ENABLED = True
-    # WTF_CSRF_SECRET_KEY = 'dsofpkoasodksap'
-    # SECRET_KEY = 'zxczxasdsad'
-    # CSRF_ENABLED = True
-    # SECRET_KEY = 'fdaskfsdlfkfasdkfmsd432423rdf'
+DEBUG = os.environ.get('DEBUG') == 'True'
+PORT = int(os.environ.get('PORT', 5000))
 
-    DEBUG = os.environ.get('DEBUG') == 'True'
-    PORT = int(os.environ.get('PORT', 5000))
-    
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + db_dir
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class ProductionConfig(Config):
-    DEBUG = True
-
-class DevelopConfig(Config):
-    DEBUG = True
-    ASSETS_DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_dir
+SQLALCHEMY_TRACK_MODIFICATIONS = False
